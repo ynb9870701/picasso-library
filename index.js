7d267927537efdde3d9cad10abaf26fd549a5b45
picasso <929144387@qq.com>
2024-03-04 14:55:00 +0800

    update fn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 // 获取随机整数
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -53,8 +55,18 @@ function delay(ms) {
 function getURLParams(url) {
   return Object.fromEntries(new URL(url).searchParams);
 }
+// 日期检测 检测当前日期是否在 date 之后
+function isAfter(currentTime, compareTime) {
+  // 使用 Day.js 解析时间
+  const current = dayjs(currentTime);
+  const compare = dayjs(compareTime);
+  
+  // 判断当前时间是否在比较时间之后
+  return current.isAfter(compare);
+}
 
-module.exports = {
+
+export default {
   getRandomInt,
   isEven,
   generateUUID,
@@ -64,5 +76,6 @@ module.exports = {
   toCamelCase,
   deepCopy,
   delay,
-  getURLParams
+  getURLParams,
+  isAfter
 };
